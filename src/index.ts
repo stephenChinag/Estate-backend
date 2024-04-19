a74bddd3c6ee8f74fb36f89f7294d25b29a94e29
StpehenChinag <stephenchinag@gmail.com>
2024-04-19 16:44:08 +0100

    food-backend

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+
+app.listen(4000, () => {
+  console.log("listening to port 4000");
+});
