6d3a9c0748adacc62bac1ab2aca3b43ab13b9f6d
StpehenChinag <stephenchinag@gmail.com>
2024-05-17 12:50:53 +0100

    dev

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import postRouter from "./router/post";
 
 //setting connection string
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
-  console.log("Connected to database");
-});
+// mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+//   console.log("Connected to database");
+// });
 
 const app = express();
 
@@ -14,10 +15,8 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use("/test", postRouter);
 // test route on that
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello" });
-});
 
 app.listen(7000, () => {
   console.log("listening to port 7000");
